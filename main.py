26af0c85cdbf5d13a6ce59f5bad4f8d4980ac1b4
srah99 <105803031+srah99@users.noreply.github.com>
2024-11-21 19:46:19 -0600

    Update main.py
    from google.cloud import aiplatform as vertexai

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from google.cloud import storage
-import vertexai from google.cloud import aiplatform
+from google.cloud import aiplatform as vertexai
 from vertexai.language_models import TextGenerationModel
 
 app = Flask(__name__)
