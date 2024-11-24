8db6d1d50749779567d19f9f6474380e5e600e47
srah99 <105803031+srah99@users.noreply.github.com>
2024-11-24 12:47:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ def chat():
     input_ids = tokenizer.encode(user_input, return_tensors='pt')
     output = model.generate(input_ids, **generation_config)
     sentences = tokenizer.batch_decode(output, skip_special_tokens=True)
-    unique_sentences = list(dict.fromkeys(sentences))
-    truncated_response = '. '.join(unique_sentences[:5])
+    response_text = ' '.join([' '.join(sentence.split()) for sentence in sentences])
+    unique_words = list(dict.fromkeys(response_text.split()))
+    truncated_response = ' '.join(unique_words[:50])  # limit to 50 words
     return jsonify({"response": truncated_response})
 
 if __name__ == "__main__":
